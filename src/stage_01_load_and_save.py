95ee0f5f0e5c2421a776eb2656fe52c710165e36
omkarpbankar <omkarpbankar@gmail.com>
2022-05-24 11:56:50 +0530

    dvc repro issue

--- src/stage_01_load_and_save.py
+++ src/stage_01_load_and_save.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import argparse 
-from src.utils.common_utils import read_params
+from src.utils.common_utils import read_params, clean_prev_dirs_if_exists,create_dir, save_local_df
 import logging
 
-def get_data(config_path:str):
+def get_data(config_path:str)->None:
     config = read_params(config_path)
-    print(config)
-    return None
+    
+    data_path = config['data_source']['s3_source']
+    artifacts_dir=config['artifacts']['artifacts_dir']
+    raw_local_data_dir = config['artifacts']['raw_local_data_dir']
+    raw_local_data = config['artifacts']['raw_local_data']
+
+    clean_prev_dirs_if_exists(artifacts_dir)
+    create_dir(dirs=[artifacts_dir,raw_local_data_dir])
+    
+    df=pd.read_csv(data_path, sep=',')
+    
+    save_local_df(df,raw_local_data,header =True)
+
 
 
 if __name__ == '__main__':
--- src/utils/common_utils.py
+++ src/utils/common_utils.py
@@ -10,3 +10,19 @@ def read_params(config_path:str) -> dict:
         config=yaml.safe_load(yaml_file)
     
     return config
+
+def clean_prev_dirs_if_exists(dir_path:str)->None:
+    if os.path.isdir(dir_path):
+        shutil.rmtree(dir_path)
+
+
+def create_dir(dirs:list)->None:
+    for dir_path in dirs:
+        os.makedirs(dir_path,exist_ok=True)
+
+def save_local_df(df,df_path,header=False):
+    if header:
+        new_cols=[col.replace(' ','_') for col in df.columns]
+        df.to_csv(df_path,index=False,header=new_cols)
+    else:
+        df.to_csv(df_path,index=False)
